9aef7464ab5976159c2c89300b9a21dad4a5d21c
danielcantillo0 <danielcantillo8@gmail.com>
2020-07-02 18:09:57 -0500

    First Commit of dragon de calle web page

---
+++ dragon-web/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import HomePage from './HomePage.component.jsx'
+import TitlePage from './TitlePage.component'
+
+function App() {
+  return (
+    <div >
+      <TitlePage/>
+      <HomePage/> 
+    </div>
+  );
+}
+
+export default App;
---
+++ dragon-web/src/TitlePage.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const TitlePage = () => (
+    <div className="titlepage">
+        <div className="title">
+            Dragon de Calle
+        </div>
+        <button className="button">SHOP</button>
+        <button className="button">INSTAGRAM</button>
+    </div>
+)
+
+export default TitlePage;
