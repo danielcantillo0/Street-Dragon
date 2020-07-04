256e7f85f164b35a317b20046c9aeab26aca8854
danielcantillo0 <danielcantillo8@gmail.com>
2020-07-04 14:06:39 -0500

    routing

--- dragon-web/src/components/directory/directory.component.jsx
+++ dragon-web/src/components/directory/directory.component.jsx
@@ -11,26 +11,37 @@ class Directory extends React.Component {
         this.state = {
             sections:[
                 {
-                    title: 'Bolsos',
-                    imageUrl:'https://i.ibb.co/ZLWKx7K/logo.png',
-                    id: 1
-                },
-                {
-                    title: 'Croptops',
-                    imageUrl:'https://i.ibb.co/LYqD28P/lightmyfire.jpg',
-                    id: 2
-                },
-                {
-                    title: 'Prints',
-                    imageUrl:'https://i.ibb.co/t2KXhgf/Emily.jpg',
-                    id: 3
-                },
-                {
-                    title: 'Camisas',
-                    imageUrl:'https://i.ibb.co/K0c1smJ/dumb.jpg',
+                    title: 'hats',
+                    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                    id: 1,
+                    linkUrl: 'hats'
+                  },
+                  {
+                    title: 'jackets',
+                    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+                    id: 2,
+                    linkUrl: ''
+                  },
+                  {
+                    title: 'sneakers',
+                    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+                    id: 3,
+                    linkUrl: ''
+                  },
+                  {
+                    title: 'womens',
+                    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
                     size: 'large',
-                    id: 4
-                },
+                    id: 4,
+                    linkUrl: ''
+                  },
+                  {
+                    title: 'mens',
+                    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+                    size: 'large',
+                    id: 5,
+                    linkUrl: ''
+                  },
                 
 
             ]
@@ -40,8 +51,8 @@ class Directory extends React.Component {
         return (
             <div className="directory-menu">
                 {
-                    this.state.sections.map(({title, imageUrl, id, size}) => (
-                        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+                    this.state.sections.map(({id, ...otherProps}) => (
+                        <MenuItem key={id} {...otherProps}  />
                     ))
                 }
             </div>
