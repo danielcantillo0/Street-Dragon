701fe725de9cd081208ca2c42a6f152c050429d4
danielcantillo0 <danielcantillo8@gmail.com>
2020-07-15 19:12:43 -0500

    database and sign up done with email and password

--- dragon-web/src/App.js
+++ dragon-web/src/App.js
@@ -6,8 +6,7 @@ import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import Header from './components/header/header.component'
-import { auth } from './firebase/firebase.utils'
-
+import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 
 class App extends React.Component {
   constructor() {
@@ -20,11 +19,25 @@ class App extends React.Component {
   unsusbscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsusbscribeFromAuth = auth.onAuthStateChanged(user => {this.setState(
-        { currentUser: user }        
-      )
-      console.log(user)    
-    })
+    this.unsusbscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+
+          console.log(this.state)
+        });
+        
+      } else {
+        this.setState({ currentUser: userAuth })
+      }
+    });
   }
 
   componentWillUnmount() {
